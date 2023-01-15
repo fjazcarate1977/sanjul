0c4061f41ea9c39b6e22e0060119b43d1dff22ad
Francisco Javier Azcárate de Castro <pacoazcarate@gmail.com>
2023-01-15 20:34:33 +0100

    feat: SANJUL-21 complete philosophy page content

--- nodejs/packages/frontend/src/data/philosophy/en-US.ts
+++ nodejs/packages/frontend/src/data/philosophy/en-US.ts
@@ -1,19 +1,23 @@
 export default `
 
-Our philosophy is quality, and to achieve it our mentality is "craftsmanship". 
+Our philosophy is **quality**, and to achieve it our mentality is ***"craftsmanship"***. 
 We like to treat each project meticulously, without delaying it.<br><br>
 
-To fulfill this ideology, we focus on having a solid architectural foundation. 
-Studying the client's needs in depth, searching for optimal solutions or carrying out proofs of concept are a fundamental part of this approach.<br><br>
+To fulfill this methodolgy, we focus on having a **solid architectural foundation**. 
+Studying the client's needs in depth, searching for optimal solutions or carrying out proofs of concept, are a fundamental part of this approach.<br><br>
 
-This makes that our work and its quality grow exponentially.<br><br>
+This makes that our work and its **quality grow exponentially**.<br><br>
 
-We like simplicity, we both grew up playing "Super Mario World", 76 levels, more than 30 characters? 512 KB! 
-The code of each application must be understandable, "readable", correctly modulated, no complex guides should be necessary, it should speak for itself. 
-The same principle applies to the necessary infrastructures, another important part never sufficiently appreciated.<br><br>
+![Philosophy Super Nintendo](/img/philosophy-snes.avif)<br><br>
 
-We face each project with humility and exigency, without settling for shortcuts or half solutions, always ready to learn every day.<br><br>
+We like **simplicity**, we grew up playing "Super Mario World", 76 levels, more than 30 characters... only 512 KB! 
+The code of each application must be **understandable**, ***"readable"***, correctly **modulated**, no complex guides should be necessary, it should speak for itself. 
+The same main ideas applies to the necessary infrastructures, another important part never enough considered.<br><br>
+
+We face each project with **humility and exigency**, without settling for shortcuts or half solutions, always ready to learn every day.<br><br>
 
 If you share our way of working, contact us!
+
+![Francisco's sign](/img/sign.avif)<br><br>
 
 `;
