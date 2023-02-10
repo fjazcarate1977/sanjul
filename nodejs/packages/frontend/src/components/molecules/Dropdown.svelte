27085d44cbea5f526d282da0ac2ef329bb73993f
Francisco Javier Azcárate de Castro <pacoazcarate@gmail.com>
2023-02-10 14:41:38 +0100

    feat: SANJUL-27 added click outside directive

--- nodejs/packages/frontend/src/components/molecules/Dropdown.svelte
+++ nodejs/packages/frontend/src/components/molecules/Dropdown.svelte
@@ -1,70 +1,81 @@
 <script lang="ts">
-  import { createPopper } from '@popperjs/core';
   import { _ } from 'svelte-i18n';
+  import { createPopperActions } from 'svelte-popperjs';
 
   import type { DropdownProps } from '$lib/types';
 
-  let dropdownPopoverShow = false;
 
-  let btnDropdownRef: Element;
-  let popoverDropdownRef: HTMLElement;
+  let dropdownPopoverShow = false;
 
   export let dropdownList: DropdownProps[];
   export let hideNavBar: () => void;
 
-  const toggleDropdown = (event: MouseEvent) => {
-    event.preventDefault();
-    if (dropdownPopoverShow) {
-      dropdownPopoverShow = false;
-    } else {
-      dropdownPopoverShow = true;
-      createPopper(btnDropdownRef, popoverDropdownRef, {
-        placement: 'bottom-start'
-      });
-    }
+  const [popperRef, popperContent] = createPopperActions({
+    placement: 'bottom-start',
+    strategy: 'fixed'
+  });
+
+  const setDropdownPopoverShow = () => {
+    dropdownPopoverShow = !dropdownPopoverShow;
   };
 
   const hideDropdown = () => {
     dropdownPopoverShow = false;
     hideNavBar();
   };
+
+  function clickOutside(node: Node) {
+    const handleClick = (event: Event) => {
+      if (node && !node.contains(event.target) && !event.defaultPrevented) {
+        node.dispatchEvent(new CustomEvent('click_outside', node));
+      }
+    };
+
+    document.addEventListener('click', handleClick, true);
+
+    return {
+      destroy() {
+        document.removeEventListener('click', handleClick, true);
+      }
+    };
+  }
 </script>
 
-<div>
+<div use:clickOutside on:click_outside={hideDropdown}>
   <a
     class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
     href="#pablo"
-    bind:this={btnDropdownRef}
-    on:click={toggleDropdown}
+    use:popperRef
+    on:click={() => setDropdownPopoverShow()}
   >
     {$_('commons.navigation')}
   </a>
-  <div
-    bind:this={popoverDropdownRef}
-    class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48 {dropdownPopoverShow
-      ? 'block'
-      : 'hidden'}"
-  >
-    {#each dropdownList as { i18nref, links }, index}
-      {#if i18nref}
-        <span
-          class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
-        >
-          {$_(i18nref)}
-        </span>
-      {/if}
-      {#each links as { link, i18nref }}
-        <a
-          href={link}
-          class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
-          on:click={hideDropdown}
-        >
-          {$_(i18nref)}
-        </a>
+  {#if dropdownPopoverShow}
+    <div
+      use:popperContent
+      class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg min-w-48"
+    >
+      {#each dropdownList as { i18nref, links }, index}
+        {#if i18nref}
+          <span
+            class="text-sm pt-2 pb-0 px-4 font-bold block w-full whitespace-nowrap bg-transparent text-blueGray-400"
+          >
+            {$_(i18nref)}
+          </span>
+        {/if}
+        {#each links as { link, i18nref }}
+          <a
+            href={link}
+            class="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
+            on:click={() => hideDropdown()}
+          >
+            {$_(i18nref)}
+          </a>
+        {/each}
+        {#if index < dropdownList.length - 1}
+          <div class="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
+        {/if}
       {/each}
-      {#if index < dropdownList.length - 1}
-        <div class="h-0 mx-4 my-2 border border-solid border-blueGray-100" />
-      {/if}
-    {/each}
-  </div>
+    </div>
+  {/if}
 </div>
