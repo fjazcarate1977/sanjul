fe88bc605534b2a26a1a01f5bc1831e8ae1e11dc
Francisco Javier Azcárate de Castro <pacoazcarate@gmail.com>
2023-02-22 13:33:03 +0100

    feat: SANJUL-33 lint warning removed & improves

--- nodejs/packages/frontend/src/components/molecules/Dropdown.svelte
+++ nodejs/packages/frontend/src/components/molecules/Dropdown.svelte
@@ -43,6 +43,7 @@
 <div use:clickOutside on:click_outside={hideDropdown}>
   <a
     class="lg:text-white lg:hover:text-blueGray-200 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold cursor-pointer"
+    href="#navigation"
     use:popperRef
     on:click={() => setDropdownPopoverShow()}
   >
